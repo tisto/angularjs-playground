7c8d2cc1163eda403da15dbe50f66beab8f1444a
Timo Stollenwerk <contact@timostollenwerk.net>
2014-03-10 09:14:44 +0100

    flowchart example added.

---
+++ flowchart/angular.js
@@ -0,0 +1,24 @@
+var flowchartModule = angular.module('FlowchartApp', []);
+
+flowchartModule.factory('flowchart',
+  ['$window', function(win) {
+    'use strict';
+    var flowchart = {
+      'nodes': []
+    };
+    flowchart.addNode = function(node) {
+      flowchart.nodes.push(node);
+    };
+    return flowchart;
+  }]
+);
+
+flowchartModule.controller('FlowchartController',
+  ['$scope','flowchart', function ($scope, flowchart) {
+    'use strict';
+    $scope.flowchart = flowchart;
+    $scope.addNode = function(node) {
+      flowchart.addNode(node);
+    };
+  }]
+);
