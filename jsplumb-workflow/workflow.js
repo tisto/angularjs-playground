b53ad644267160af26c16e10db50681b6c63e826
tisto <tisto@plone.org>
2014-03-12 22:19:11 +0100

    Add label inline editing.

--- jsplumb-workflow/workflow.js
+++ jsplumb-workflow/workflow.js
@@ -29,7 +29,7 @@ myModule.run(function(flowchart) {
         {
           label: 'Click to edit',
           id: 'label',
-          cssClass:'aLabel',
+          cssClass:'aLabel connectionLabel',
           location: 0.7,
         }
       ]
@@ -86,9 +86,9 @@ myModule.controller('MyController',
       // bind a click listener to each connection; the connection is deleted. you could of course
       // just do this: jsPlumb.bind('click', jsPlumb.detach), but I wanted to make it clear what was
       // happening.
-      instance.bind('click', function(c) {
-        instance.detach(c);
-      });
+      //instance.bind('click', function(c) {
+      //  instance.detach(c);
+      //});
 
       // bind a connection listener. note that the parameter passed to this function contains more than
       // just the new connection - see the documentation for a full list of what is included in 'info'.
@@ -96,6 +96,42 @@ myModule.controller('MyController',
       // id as the label overlay's text.
       instance.bind('connection', function(info) {
         info.connection.getOverlay('label').setLabel(info.connection.id);
+      });
+
+      // Inline Editing Input
+      function inline_edit_input(object) {
+        var label = object;
+        var value = label.text();
+        label.empty();
+        label.append('<input type="text" value="' + value + '" data-value-orig="' + value + '" />')
+          .append('<div class="actions"><button class="btn btn-xs btn-default">Abbrechen</button><button class="btn btn-xs btn-primary">Speichern</button></div>');
+      }
+
+      function inline_edit_input_save(object) {
+        var label = object.parent().parent();
+        var value = label.find('input').val();
+        label.empty();
+        label.append(value);
+      }
+
+      function inline_edit_input_cancel(object) {
+        var label = object.parent().parent();
+        var value = label.find('input').attr('data-value-orig');
+        label.empty();
+        label.append(value);
+      }
+
+      // Connection Label Inline Editing
+      $(document).on("dblclick", ".connectionLabel", function (event) {
+        inline_edit_input($(this));
+      });
+      // Save
+      $(document).on("click", ".connectionLabel .btn-primary", function (event) {
+        inline_edit_input_save($(this));
+      });
+      // Cancel
+      $(document).on("click", ".connectionLabel .btn-default", function (event) {
+        inline_edit_input_cancel($(this));
       });
 
       instance.doWhileSuspended(function() {
