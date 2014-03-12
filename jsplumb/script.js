42f5629ea8e526255e187106f60b45fadfc33971
Timo Stollenwerk <contact@timostollenwerk.net>
2014-03-12 09:08:22 +0100

    Create instance within run and add connections in the controller.

--- jsplumb/script.js
+++ jsplumb/script.js
@@ -1,29 +1,40 @@
 var myModule = angular.module('myApp', []);
 
+myModule.factory('flowchart', function() {
+  var flowchart = null;
+
+  return {
+    get: function() { return flowchart; },
+    set: function(fc) { flowchart = fc; }
+  };
+});
+
+myModule.run(function(flowchart) {
+  instance = jsPlumb.getInstance({
+    DragOptions : { cursor: 'pointer', zIndex:2000 },
+    ConnectionOverlays : [
+      [ 'Arrow', { location: 0.99 } ],
+      [ 'Label',
+        {
+          location: 0.2,
+          id: 'label',
+          cssClass: 'aLabel connectionLabel'
+        }
+      ]
+    ],
+    Container:'flowchart'
+  });
+  flowchart.set(instance);
+});
+
 myModule.controller('MyController',
-  function($scope) {
+  function($scope, flowchart) {
     'use strict';
-    var instance;
-    angular.element(document).ready(function () {
-      instance = jsPlumb.getInstance({
-        DragOptions : { cursor: 'pointer', zIndex:2000 },
-        ConnectionOverlays : [
-          [ 'Arrow', { location: 0.99 } ],
-          [ 'Label',
-            {
-              location: 0.2,
-              id: 'label',
-              cssClass: 'aLabel connectionLabel'
-            }
-          ]
-        ],
-        Container:'flowchart'
-      });
-      var e0 = instance.addEndpoint('container0');
-      var e1 = instance.addEndpoint('container1');
-      instance.connect({ source:e0, target:e1 });
-    });
-    $scope.flowchart = instance;
+    $scope.flowchart = flowchart;
+    instance = $scope.flowchart.get();
+    var e0 = instance.addEndpoint('container0');
+    var e1 = instance.addEndpoint('container1');
+    instance.connect({ source:e0, target:e1 });
   }
 );
 
