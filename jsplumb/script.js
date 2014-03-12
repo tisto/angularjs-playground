0ef6a25e8417595837d7ed8db03314cc3ff68210
Timo Stollenwerk <contact@timostollenwerk.net>
2014-03-12 14:39:29 +0100

    Fix node/endpoint offset.

--- jsplumb/script.js
+++ jsplumb/script.js
@@ -1,5 +1,40 @@
 var myModule = angular.module('myApp', []);
 
+var flowchartEndpointOptions = {
+  endpoint:"Dot",
+  isSource:true,
+  isTarget:true,
+  maxConnections:-1,
+  paintStyle:{
+    strokeStyle: "#7AB02C",
+    fillStyle: "#7AB02C",
+    radius: 3,
+    lineWidth: 3
+  },
+  hoverPaintStyle: {
+    fillStyle: "#216477",
+    strokeStyle: "#216477"
+  },
+  connector: [
+    "Flowchart",
+    { stub:[40, 60], gap:10, cornerRadius:5, alwaysRespectStubs:true }
+  ],
+  connectorStyle: {
+    lineWidth: 3,
+    strokeStyle: "black",
+    joinstyle: "round",
+    outlineWidth: 2,
+    outlineColor: "white"
+  },
+  connectorHoverStyle: {
+    lineWidth: 4,
+    strokeStyle: "black",
+    outlineWidth: 2,
+    outlineColor: "white"
+  },
+  dropOptions:{ hoverClass:"hover", activeClass:"active" },
+};
+
 myModule.factory('flowchart', function() {
   var flowchart = null;
 
@@ -32,11 +67,27 @@ myModule.controller('MyController',
     'use strict';
     $scope.flowchart = flowchart;
     instance = $scope.flowchart.get();
-    var e0 = instance.addEndpoint('container0');
-    var e1 = instance.addEndpoint('container1');
-    instance.connect({ source:e0, target:e1 });
+
+    var _addEndpoints = function(toId) {
+      var anchors = ["TopCenter", "BottomCenter", "LeftMiddle", "RightMiddle"];
+      for (var i = 0; i < anchors.length; i++) {
+        var sourceUUID = toId + anchors[i];
+        instance.addEndpoint(
+          toId,
+          flowchartEndpointOptions,
+          {
+            anchor:anchors[i],
+            uuid:sourceUUID
+          }
+        );
+      }
+    };
+
     instance.doWhileSuspended(function() {
-      instance.draggable(jsPlumb.getSelector("#flowchart div"), { grid: [20, 20] });
+      instance.draggable($(".node"), { grid: [20, 20] });
+      _addEndpoints('container0');
+      _addEndpoints('container1');
+      _addEndpoints('container2');
     });
   }
 );
