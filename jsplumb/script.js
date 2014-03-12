930902122abf77036cd44e1dd29d6623edf36f15
Timo Stollenwerk <contact@timostollenwerk.net>
2014-03-12 08:53:30 +0100

    Minor.

--- jsplumb/script.js
+++ jsplumb/script.js
@@ -8,19 +8,19 @@ myModule.controller('MyController',
       instance = jsPlumb.getInstance({
         DragOptions : { cursor: 'pointer', zIndex:2000 },
         ConnectionOverlays : [
-          [ "Arrow", { location: 0.99 } ],
-          [ "Label",
+          [ 'Arrow', { location: 0.99 } ],
+          [ 'Label',
             {
               location: 0.2,
-              id: "label",
-              cssClass: "aLabel connectionLabel"
+              id: 'label',
+              cssClass: 'aLabel connectionLabel'
             }
           ]
         ],
-        Container:"flowchart"
+        Container:'flowchart'
       });
-      var e0 = instance.addEndpoint("container0");
-      var e1 = instance.addEndpoint("container1");
+      var e0 = instance.addEndpoint('container0');
+      var e1 = instance.addEndpoint('container1');
       instance.connect({ source:e0, target:e1 });
     });
     $scope.flowchart = instance;
