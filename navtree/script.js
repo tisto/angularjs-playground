39379fbe3d5629dbf500789eb734fcc717751c33
Timo Stollenwerk <contact@timostollenwerk.net>
2014-03-18 10:02:34 +0100

    Factor out example_treedata.

--- navtree/script.js
+++ navtree/script.js
@@ -4,7 +4,75 @@ deps = ['angularBootstrapNavTree', 'ngAnimate'];
 
 app = angular.module('AbnTest', deps);
 
+var example_treedata = [
+  {
+    label: 'Animal',
+    children: [
+      {
+        label: 'Dog',
+        data: {
+          description: "man's best friend"
+        }
+      }, {
+        label: 'Cat',
+        data: {
+          description: "Felis catus"
+        }
+      }, {
+        label: 'Hippopotamus',
+        data: {
+          description: "hungry, hungry"
+        }
+      }, {
+        label: 'Chicken',
+        children: ['White Leghorn', 'Rhode Island Red', 'Jersey Giant']
+      }
+    ]
+  }, {
+    label: 'Vegetable',
+    children: [
+      {
+        label: 'Oranges'
+      }, {
+        label: 'Apples',
+        children: [
+          {
+            label: 'Granny Smith'
+          }, {
+            label: 'Red Delicous'
+          }, {
+            label: 'Fuji'
+          }
+        ]
+      }
+    ]
+  }, {
+    label: 'Mineral',
+    children: [
+      {
+        label: 'Rock',
+        children: ['Igneous', 'Sedimentary', 'Metamorphic']
+      }, {
+        label: 'Metal',
+        children: ['Aluminum', 'Steel', 'Copper']
+      }, {
+        label: 'Plastic',
+        children: [
+          {
+            label: 'Thermoplastic',
+            children: ['polyethylene', 'polypropylene', 'polystyrene', ' polyvinyl chloride']
+          }, {
+            label: 'Thermosetting Polymer',
+            children: ['polyester', 'polyurethane', 'vulcanized rubber', 'bakelite', 'urea-formaldehyde']
+          }
+        ]
+      }
+    ]
+  }
+];
+
 app.controller('AbnTestController', function($scope) {
+  'use strict';
   var apple_selected;
   $scope.my_tree_handler = function(branch) {
     var _ref;
@@ -16,80 +84,5 @@ app.controller('AbnTestController', function($scope) {
   apple_selected = function(branch) {
     return $scope.output = "APPLE! : " + branch.label;
   };
-  $scope.example_treedata = [
-    {
-      label: 'Animal',
-      children: [
-        {
-          label: 'Dog',
-          data: {
-            description: "man's best friend"
-          }
-        }, {
-          label: 'Cat',
-          data: {
-            description: "Felis catus"
-          }
-        }, {
-          label: 'Hippopotamus',
-          data: {
-            description: "hungry, hungry"
-          }
-        }, {
-          label: 'Chicken',
-          children: ['White Leghorn', 'Rhode Island Red', 'Jersey Giant']
-        }
-      ]
-    }, {
-      label: 'Vegetable',
-      data: {
-        definition: "A plant or part of a plant used as food, typically as accompaniment to meat or fish, such as a cabbage, potato, carrot, or bean.",
-        data_can_contain_anything: true
-      },
-      onSelect: function(branch) {
-        return $scope.output = "Vegetable: " + branch.data.definition;
-      },
-      children: [
-        {
-          label: 'Oranges'
-        }, {
-          label: 'Apples',
-          children: [
-            {
-              label: 'Granny Smith',
-              onSelect: apple_selected
-            }, {
-              label: 'Red Delicous',
-              onSelect: apple_selected
-            }, {
-              label: 'Fuji',
-              onSelect: apple_selected
-            }
-          ]
-        }
-      ]
-    }, {
-      label: 'Mineral',
-      children: [
-        {
-          label: 'Rock',
-          children: ['Igneous', 'Sedimentary', 'Metamorphic']
-        }, {
-          label: 'Metal',
-          children: ['Aluminum', 'Steel', 'Copper']
-        }, {
-          label: 'Plastic',
-          children: [
-            {
-              label: 'Thermoplastic',
-              children: ['polyethylene', 'polypropylene', 'polystyrene', ' polyvinyl chloride']
-            }, {
-              label: 'Thermosetting Polymer',
-              children: ['polyester', 'polyurethane', 'vulcanized rubber', 'bakelite', 'urea-formaldehyde']
-            }
-          ]
-        }
-      ]
-    }
-  ];
+  $scope.example_treedata = example_treedata;
 });
