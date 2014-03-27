f94b170148cd9d79db46f2ed405345ab9d0055ee
Timo Stollenwerk <contact@timostollenwerk.net>
2014-03-27 12:34:35 +0100

    Print out some more information.

--- jsonp/script.js
+++ jsonp/script.js
@@ -3,7 +3,14 @@ var myModule = angular.module('myApp', []);
 myModule.controller('TextController',
   function($scope, $http) {
     'use strict';
-    var url = 'http://demo.piwik.org/?module=API&method=VisitsSummary.getVisits&idSite=3&period=day&date=last10&format=json&jsoncallback=JSON_CALLBACK';
+    var url = 'http://demo.piwik.org/?' +
+      'module=API&' +
+      'method=Live.getLastVisitsDetails&' +
+      'idSite=3&' +
+      'period=day&' +
+      'date=yesterday&' +
+      'format=json&' +
+      'jsoncallback=JSON_CALLBACK';
     $http.jsonp(url).success(function(data, status, headers, config) {
       $scope.items = data;
     });
