d537026e65a803b9dcc66abeb9a445f1098f6bcf
tisto <tisto@plone.org>
2014-04-04 21:49:59 +0200

    Add jenkins initial yeoman structure.

---
+++ jenkins/app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular
+  .module('jenkinsApp', [
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
