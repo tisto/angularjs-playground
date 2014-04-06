3698b812fad018e8d613c076348d05505f914418
tisto <tisto@plone.org>
2014-04-06 17:41:30 +0200

    Use jenkins.plone.org as backend.

--- jenkins/app/scripts/controllers/main.js
+++ jenkins/app/scripts/controllers/main.js
@@ -2,7 +2,7 @@
 
 angular.module('jenkinsApp')
   .controller('MainCtrl', function ($scope, $http) {
-    var url = 'http://127.0.0.1:9999/job/job1/api/json?depth=1&tree=builds[url,id,number,result]';
+    var url = 'http://jenkins.plone.org/job/plone-5.0-python-2.7/api/json?tree=builds[id,url,number,result,actions[failCount,totalCount]]'
     $http.get(url).success(function(data, status, headers, config) {
       $scope.data = data;
     });
--- jenkins/app/scripts/directives/main.js
+++ jenkins/app/scripts/directives/main.js
@@ -3,7 +3,7 @@
 angular.module('jenkinsApp').directive('buildDirective',
   function() {
     return {
-      template: '<a href="{{build.url}}">{{build.number}}: {{build.result}}</a>'
+      template: '<a href="{{build.url}}">#{{build.number}}: {{build.result}} ({{build.actions[8]}}, {{build.actions[9]}})</a>'
     };
   }
 );
