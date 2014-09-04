d0e90158755a97de02bd09a7e9202d15e022b169
Timo Stollenwerk <contact@timostollenwerk.net>
2014-09-04 13:03:00 +0200

    Add backendService and dummy backend data.

--- ui-bootstrap-pagination/app.js
+++ ui-bootstrap-pagination/app.js
@@ -1,17 +1,92 @@
-angular.module('myModule', ['ui.bootstrap']);
-var PaginationDemoCtrl = function ($scope) {
-  $scope.totalItems = 64;
-  $scope.currentPage = 4;
+var myModule = angular.module('myApp', ['ui.bootstrap', 'ngMockE2E']);
 
-  $scope.setPage = function (pageNo) {
-    $scope.currentPage = pageNo;
-  };
+myModule.run(function($httpBackend) {
+  results = [
+    {
+      id: 'lorem1',
+      title: 'Lorem 1'
+    },
+    {
+      id: 'lorem2',
+      title: 'Lorem 2'
+    },
+    {
+      id: 'lorem3',
+      title: 'Lorem 3'
+    },
+    {
+      id: 'lorem4',
+      title: 'Lorem 4'
+    },
+    {
+      id: 'lorem5',
+      title: 'Lorem 5'
+    },
+    {
+      id: 'lorem6',
+      title: 'Lorem 6'
+    },
+    {
+      id: 'lorem7',
+      title: 'Lorem 7'
+    },
+    {
+      id: 'lorem8',
+      title: 'Lorem 8'
+    },
+    {
+      id: 'lorem9',
+      title: 'Lorem 9'
+    },
+    {
+      id: 'lorem10',
+      title: 'Lorem 10'
+    },
+  ];
+  $httpBackend.whenGET('/results').respond(results);
+});
 
-  $scope.pageChanged = function() {
-    console.log('Page changed to: ' + $scope.currentPage);
-  };
+myModule.factory('backendService',
+  function($http) {
+    'use strict';
+    var runUserRequest = function() {
+      return $http({
+        method: 'GET',
+        url: '/results'
+      });
+    };
+    return {
+      events: function(batchStart, batchSize) {
+        return runUserRequest();
+      }
+    };
+  }
+);
 
-  $scope.maxSize = 5;
-  $scope.bigTotalItems = 175;
-  $scope.bigCurrentPage = 1;
-};
+myModule.controller('PaginationDemoCtrl',
+  function($scope, $timeout, backendService) {
+    'use strict';
+    var timeout;
+    var batchStart = 0;
+    var batchSize = 5;
+    $scope.results = ['foo'];
+    backendService.events(batchStart, batchSize)
+    .success(function(data, status) {
+      $scope.results = data;
+    });
+    $scope.totalItems = 64;
+    $scope.currentPage = 4;
+
+    $scope.setPage = function (pageNo) {
+      $scope.currentPage = pageNo;
+    };
+
+    $scope.pageChanged = function() {
+      console.log('Page changed to: ' + $scope.currentPage);
+    };
+
+    $scope.maxSize = 5;
+    $scope.bigTotalItems = 175;
+    $scope.bigCurrentPage = 1;
+  }
+);
