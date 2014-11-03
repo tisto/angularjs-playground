931d1aad17852d7cbefe3ea1b18ee58ff50103b4
Timo Stollenwerk <contact@timostollenwerk.net>
2014-11-03 13:27:31 +0100

    Log new date values to the console.

--- daterangepicker/app.js
+++ daterangepicker/app.js
@@ -4,5 +4,13 @@ myModule.controller('DaterangeController',
   function($scope) {
     'use strict';
     $scope.date = {startDate: null, endDate: null};
+
+    $scope.$watch('date', function(newValue, oldValue) {
+      var startDate = newValue.startDate;
+      var endDate = newValue.endDate;
+      console.log("Start: " + startDate);
+      console.log("End: " + endDate);
+    });
   }
 );
+
