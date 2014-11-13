7804c6a19ec739bcc9118c6858b0c81f9ad37ff0
Timo Stollenwerk <contact@timostollenwerk.net>
2014-11-13 11:28:09 +0100

    Add gulp browser-sync. Add basic line chart with static dummy data.

--- google-charts-daterange-line-chart/app.js
+++ google-charts-daterange-line-chart/app.js
@@ -1,10 +1,131 @@
-var myModule = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['googlechart']);
 
-myModule.controller('TextController',
+var chartColorBlue = '#a5bee6';
+var chartColorYellow = '#f0c155';
+var chartColorRed = '#e5725f';
+var chartColorGreen = '#70b654';
+var chartColorLighterGrey = '#ADADAD';
+var chartColorLightGrey = '#999999';
+var chartColorDarkGrey = '#666666';
+var chartFontSize = 10;
+
+
+myApp.controller('DateRangeLineCharController', ['$scope',
   function($scope) {
-    'use strict';
-    var someText = {};
-    someText.message = 'Hi angular world!';
-    $scope.someText = someText;
-  }
+
+    $scope.chart = {};
+
+    $scope.chart.data = {
+      "cols": [
+        {
+            "id": "zeit",
+            "label": "Zeit",
+            "type": "string"
+        },
+        {
+            "id": "anfragen",
+            "label": "Anfragen",
+            "type": "number"
+        }
+      ],
+      "rows": [
+        {
+            "c": [
+                {
+                    "v": "07/11/2014"
+                },
+                {
+                    "v": 5
+                }
+            ]
+        },
+        {
+            "c": [
+                {
+                    "v": "08/11/2014"
+                },
+                {
+                    "v": 0
+                }
+            ]
+        },
+        {
+            "c": [
+                {
+                    "v": "09/11/2014"
+                },
+                {
+                    "v": 0
+                }
+            ]
+        },
+        {
+            "c": [
+                {
+                    "v": "10/11/2014"
+                },
+                {
+                    "v": 30
+                }
+            ]
+        },
+        {
+            "c": [
+                {
+                    "v": "11/11/2014"
+                },
+                {
+                    "v": 0
+                }
+            ]
+        },
+        {
+            "c": [
+                {
+                    "v": "12/11/2014"
+                },
+                {
+                    "v": 0
+                }
+            ]
+        },
+        {
+            "c": [
+                {
+                    "v": "13/11/2014"
+                },
+                {
+                    "v": 0
+                }
+            ]
+        }
+      ]
+    }
+
+    $scope.chart.type = "LineChart";
+    $scope.chart.options = {
+      fontSize: chartFontSize,
+      pointSize: 6,
+      legend: {position: 'top'},
+      vAxis: {
+        textStyle: {
+          color: chartColorDarkGrey,
+          fontSize: chartFontSize,
+        },
+        gridlines: {color: chartColorLighterGrey, count: 5},
+        minValue: 0,
+      },
+      hAxis: {
+        textStyle: {
+          color: chartColorLightGrey,
+          fontSize: chartFontSize,
+        },
+      },
+      animation:{
+        duration: 1000,
+        easing: 'out'
+      },
+    };
+
+  }]
 );
--- google-charts-daterange-line-chart/gulpfile.js
+++ google-charts-daterange-line-chart/gulpfile.js
@@ -1,11 +1,27 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync');
 
-// Static server
+
+// --- WATCH -----------------------------------------------------------------
+gulp.task('watch', function() {
+  gulp.watch('**/*.*', ['browser-sync']);
+});
+
+
+// --- BROWSER SYNC ----------------------------------------------------------
 gulp.task('browser-sync', function() {
     browserSync({
         server: {
             baseDir: "./"
         }
     });
+});
+gulp.task('browser-sync-reload', function () {
+    browserSync.reload();
+});
+
+
+// --- DEFAULT ---------------------------------------------------------------
+gulp.task('default', ['browser-sync'], function () {
+    gulp.watch("*.*", ['browser-sync-reload']);
 });
