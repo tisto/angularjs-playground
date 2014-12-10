923a8c3e499b079fae2755bbcd0e4a8ffbf3a856
Timo Stollenwerk <contact@timostollenwerk.net>
2014-12-10 11:38:15 +0100

    Use js closures.

--- google-charts-daterange-line-chart/app.js
+++ google-charts-daterange-line-chart/app.js
@@ -1,88 +1,92 @@
-var myApp = angular.module('myApp', ['googlechart', 'daterangepicker']);
+(function() {
+  'use strict';
 
-var chartColorBlue = '#a5bee6';
-var chartColorYellow = '#f0c155';
-var chartColorRed = '#e5725f';
-var chartColorGreen = '#70b654';
-var chartColorLighterGrey = '#ADADAD';
-var chartColorLightGrey = '#999999';
-var chartColorDarkGrey = '#666666';
-var chartFontSize = 10;
+  var myApp = angular.module('myApp', ['googlechart', 'daterangepicker']);
 
-
-myApp.factory('chartService', ['$http',
-  function($http) {
-    'use strict';
-    var getTickets = function(daterange) {
-      return $http({
-        method: 'GET',
-        url: 'get_tickets?daterange=' + daterange
-      });
-    };
-    return {
-      getTickets: function(daterange) {
-        return getTickets(daterange);
-      }
-    };
-  }
-]);
+  var chartColorBlue = '#a5bee6';
+  var chartColorYellow = '#f0c155';
+  var chartColorRed = '#e5725f';
+  var chartColorGreen = '#70b654';
+  var chartColorLighterGrey = '#ADADAD';
+  var chartColorLightGrey = '#999999';
+  var chartColorDarkGrey = '#666666';
+  var chartFontSize = 10;
 
 
-myApp.controller('DateRangeLineCharController', ['$scope', '$timeout', 'chartService',
-  function($scope, $timeout, chartService) {
-
-    // Chart
-    $scope.chart = {};
-    $scope.chart.type = "LineChart";
-    $scope.chart.options = {
-      fontSize: chartFontSize,
-      pointSize: 6,
-      legend: {position: 'top'},
-      vAxis: {
-        textStyle: {
-          color: chartColorDarkGrey,
-          fontSize: chartFontSize,
-        },
-        gridlines: {color: chartColorLighterGrey, count: 5},
-        minValue: 0,
-      },
-      hAxis: {
-        textStyle: {
-          color: chartColorLightGrey,
-          fontSize: chartFontSize,
-        },
-      },
-      animation:{
-        duration: 1000,
-        easing: 'out'
-      },
-    };
-
-    // Chart Data
-    var getTickets = function(daterange) {
-      var timeout;
-      timeout = $timeout(function() {
-        chartService.getTickets(daterange)
-        .success(function(data, status) {
-          $scope.chart.data = data;
-        })
-        .error(function(data, status) {
-          alert('chartService.getTickets() failed: ' + data);
+  myApp.factory('chartService', ['$http',
+    function($http) {
+      var getTickets = function(daterange) {
+        return $http({
+          method: 'GET',
+          url: 'get_tickets?daterange=' + daterange
         });
-      }, 350);
-    };
-    getTickets('day');
-
-    // Set Daterange
-    $scope.setDaterange = function(dateRange) {
-      $scope.date = {
-        startDate: moment().subtract(7, dateRange + 's'),
-        endDate: moment(),
-        dateRange: dateRange
-      }
-      getTickets(dateRange);
+      };
+      return {
+        getTickets: function(daterange) {
+          return getTickets(daterange);
+        }
+      };
     }
-    $scope.setDaterange('day');
+  ]);
 
-  }]
-);
+
+  myApp.controller('DateRangeLineCharController', ['$scope', '$timeout', 'chartService',
+    function($scope, $timeout, chartService) {
+
+      // Chart
+      $scope.chart = {};
+      $scope.chart.type = "LineChart";
+      $scope.chart.options = {
+        fontSize: chartFontSize,
+        pointSize: 6,
+        legend: {position: 'top'},
+        vAxis: {
+          textStyle: {
+            color: chartColorDarkGrey,
+            fontSize: chartFontSize,
+          },
+          gridlines: {color: chartColorLighterGrey, count: 5},
+          minValue: 0,
+        },
+        hAxis: {
+          textStyle: {
+            color: chartColorLightGrey,
+            fontSize: chartFontSize,
+          },
+        },
+        animation:{
+          duration: 1000,
+          easing: 'out'
+        },
+      };
+
+      // Chart Data
+      var getTickets = function(daterange) {
+        var timeout;
+        timeout = $timeout(function() {
+          chartService.getTickets(daterange)
+          .success(function(data, status) {
+            $scope.chart.data = data;
+          })
+          .error(function(data, status) {
+            alert('chartService.getTickets() failed: ' + data);
+          });
+        }, 350);
+      };
+      getTickets('day');
+
+      // Set Daterange
+      $scope.setDaterange = function(dateRange) {
+        $scope.date = {
+          startDate: moment().subtract(7, dateRange + 's'),
+          endDate: moment(),
+          dateRange: dateRange
+        };
+        getTickets(dateRange);
+      };
+      $scope.setDaterange('day');
+
+    }]
+  );
+
+})();
--- google-charts-daterange-line-chart/mock-backend.js
+++ google-charts-daterange-line-chart/mock-backend.js
@@ -1,60 +1,11 @@
-var myApp;
+(function() {
+  'use strict';
 
-angular.module('e2e-mocks', ['ngMockE2E']).run(function($httpBackend) {
+  angular.module('e2e-mocks', ['ngMockE2E']).run(function($httpBackend) {
 
-  // -- TICKETS --------------------------------------------------------------
-  var day_tickets = {
-    "cols": [
-      {
-          "id": "zeit",
-          "label": "Zeit",
-          "type": "string"
-      },
-      {
-          "id": "anfragen",
-          "label": "Anfragen",
-          "type": "number"
-      }
-    ],
-    "rows": [
-      {"c": [{"v": "07.11.2014"}, {"v": 5}]},
-      {"c": [{"v": "08.11.2014"}, {"v": 0}]},
-      {"c": [{"v": "09.11.2014"}, {"v": 0}]},
-      {"c": [{"v": "10.11.2014"}, {"v": 30}]},
-      {"c": [{"v": "11.11.2014"}, {"v": 0}]},
-      {"c": [{"v": "12.11.2014"}, {"v": 0}]},
-      {"c": [{"v": "13.11.2014"}, {"v": 0}]}
-    ]
-  };
-  $httpBackend.whenGET('get_tickets?daterange=day').respond(day_tickets);
-
-  var day_tickets = {
-    "cols": [
-      {
-          "id": "zeit",
-          "label": "Zeit",
-          "type": "string"
-      },
-      {
-          "id": "anfragen",
-          "label": "Anfragen",
-          "type": "number"
-      }
-    ],
-    "rows": [
-      {"c": [{"v": "11.11.2014"}, {"v": 5}]},
-      {"c": [{"v": "12.11.2014"}, {"v": 0}]},
-      {"c": [{"v": "13.11.2014"}, {"v": 0}]},
-      {"c": [{"v": "14.11.2014"}, {"v": 30}]},
-      {"c": [{"v": "15.11.2014"}, {"v": 0}]},
-      {"c": [{"v": "16.11.2014"}, {"v": 0}]},
-      {"c": [{"v": "17.11.2014"}, {"v": 0}]}
-    ]
-  };
-  $httpBackend.whenGET('get_tickets?daterange=day&startDate=2014-11-11&endDate=2014-11-17').respond(day_tickets);
-
-  var week_tickets = {
-    "cols": [
+    // -- TICKETS --------------------------------------------------------------
+    var day_tickets = {
+      "cols": [
         {
             "id": "zeit",
             "label": "Zeit",
@@ -65,21 +16,21 @@ angular.module('e2e-mocks', ['ngMockE2E']).run(function($httpBackend) {
             "label": "Anfragen",
             "type": "number"
         }
-    ],
-    "rows": [
-      {"c": [{"v": 41}, {"v": 139}]},
-      {"c": [{"v": 42}, {"v": 193}]},
-      {"c": [{"v": 43}, {"v": 160}]},
-      {"c": [{"v": 44}, {"v": 408}]},
-      {"c": [{"v": 45}, {"v": 153}]},
-      {"c": [{"v": 46}, {"v": 97}]},
-      {"c": [{"v": 47}, {"v": 0}]}
-    ]
-  };
-  $httpBackend.whenGET('get_tickets?daterange=week').respond(week_tickets);
+      ],
+      "rows": [
+        {"c": [{"v": "07.11.2014"}, {"v": 5}]},
+        {"c": [{"v": "08.11.2014"}, {"v": 0}]},
+        {"c": [{"v": "09.11.2014"}, {"v": 0}]},
+        {"c": [{"v": "10.11.2014"}, {"v": 30}]},
+        {"c": [{"v": "11.11.2014"}, {"v": 0}]},
+        {"c": [{"v": "12.11.2014"}, {"v": 0}]},
+        {"c": [{"v": "13.11.2014"}, {"v": 0}]}
+      ]
+    };
+    $httpBackend.whenGET('get_tickets?daterange=day').respond(day_tickets);
 
-  var month_tickets = {
-    "cols": [
+    var day_tickets = {
+      "cols": [
         {
             "id": "zeit",
             "label": "Zeit",
@@ -90,45 +41,96 @@ angular.module('e2e-mocks', ['ngMockE2E']).run(function($httpBackend) {
             "label": "Anfragen",
             "type": "number"
         }
-    ],
-    "rows": [
-      {"c": [{"v": "2014, 2"}, {"v": 139}]},
-      {"c": [{"v": "2014, 3"}, {"v": 193}]}
-    ]
-  };
-  $httpBackend.whenGET('get_tickets?daterange=month').respond(month_tickets);
+      ],
+      "rows": [
+        {"c": [{"v": "11.11.2014"}, {"v": 5}]},
+        {"c": [{"v": "12.11.2014"}, {"v": 0}]},
+        {"c": [{"v": "13.11.2014"}, {"v": 0}]},
+        {"c": [{"v": "14.11.2014"}, {"v": 30}]},
+        {"c": [{"v": "15.11.2014"}, {"v": 0}]},
+        {"c": [{"v": "16.11.2014"}, {"v": 0}]},
+        {"c": [{"v": "17.11.2014"}, {"v": 0}]}
+      ]
+    };
+    $httpBackend.whenGET('get_tickets?daterange=day&startDate=2014-11-11&endDate=2014-11-17').respond(day_tickets);
 
-  var year_tickets = {
-    "cols": [
-        {
-            "id": "zeit",
-            "label": "Zeit",
-            "type": "string"
-        },
-        {
-            "id": "anfragen",
-            "label": "Anfragen",
-            "type": "number"
-        }
-    ],
-    "rows": [
-      {"c": [{"v": "2007"}, {"v": 9}]},
-      {"c": [{"v": "2008"}, {"v": 83}]},
-      {"c": [{"v": "2009"}, {"v": 96}]},
-      {"c": [{"v": "2010"}, {"v": 52}]},
-      {"c": [{"v": "2011"}, {"v": 193}]},
-      {"c": [{"v": "2012"}, {"v": 293}]},
-      {"c": [{"v": "2013"}, {"v": 194}]},
-      {"c": [{"v": "2014"}, {"v": 139}]},
-    ]
-  };
-  $httpBackend.whenGET('get_tickets?daterange=year').respond(year_tickets);
+    var week_tickets = {
+      "cols": [
+          {
+              "id": "zeit",
+              "label": "Zeit",
+              "type": "string"
+          },
+          {
+              "id": "anfragen",
+              "label": "Anfragen",
+              "type": "number"
+          }
+      ],
+      "rows": [
+        {"c": [{"v": 41}, {"v": 139}]},
+        {"c": [{"v": 42}, {"v": 193}]},
+        {"c": [{"v": 43}, {"v": 160}]},
+        {"c": [{"v": 44}, {"v": 408}]},
+        {"c": [{"v": 45}, {"v": 153}]},
+        {"c": [{"v": 46}, {"v": 97}]},
+        {"c": [{"v": 47}, {"v": 0}]}
+      ]
+    };
+    $httpBackend.whenGET('get_tickets?daterange=week').respond(week_tickets);
 
-  // --- PASS THROUGH TEMPLATES ----------------------------------------------
-  var templates_re = new RegExp('.*.html$');
-  $httpBackend.whenGET(templates_re).passThrough();
+    var month_tickets = {
+      "cols": [
+          {
+              "id": "zeit",
+              "label": "Zeit",
+              "type": "string"
+          },
+          {
+              "id": "anfragen",
+              "label": "Anfragen",
+              "type": "number"
+          }
+      ],
+      "rows": [
+        {"c": [{"v": "2014, 2"}, {"v": 139}]},
+        {"c": [{"v": "2014, 3"}, {"v": 193}]}
+      ]
+    };
+    $httpBackend.whenGET('get_tickets?daterange=month').respond(month_tickets);
 
-});
+    var year_tickets = {
+      "cols": [
+          {
+              "id": "zeit",
+              "label": "Zeit",
+              "type": "string"
+          },
+          {
+              "id": "anfragen",
+              "label": "Anfragen",
+              "type": "number"
+          }
+      ],
+      "rows": [
+        {"c": [{"v": "2007"}, {"v": 9}]},
+        {"c": [{"v": "2008"}, {"v": 83}]},
+        {"c": [{"v": "2009"}, {"v": 96}]},
+        {"c": [{"v": "2010"}, {"v": 52}]},
+        {"c": [{"v": "2011"}, {"v": 193}]},
+        {"c": [{"v": "2012"}, {"v": 293}]},
+        {"c": [{"v": "2013"}, {"v": 194}]},
+        {"c": [{"v": "2014"}, {"v": 139}]},
+      ]
+    };
+    $httpBackend.whenGET('get_tickets?daterange=year').respond(year_tickets);
 
-myApp.requires.push('e2e-mocks');
+    // --- PASS THROUGH TEMPLATES ----------------------------------------------
+    var templates_re = new RegExp('.*.html$');
+    $httpBackend.whenGET(templates_re).passThrough();
 
+  });
+
+  angular.module('myApp').requires.push('e2e-mocks');
+
+})();
