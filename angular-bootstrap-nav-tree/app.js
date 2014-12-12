7d5caa99c8a8dee7bd1696c5c11bcdd85f637741
Timo Stollenwerk <contact@timostollenwerk.net>
2014-12-12 12:42:39 +0100

    Fix angular-bootstrap-nav example.

--- angular-bootstrap-nav-tree/app.js
+++ angular-bootstrap-nav-tree/app.js
@@ -1,96 +1,106 @@
 (function() {
-  'use strict';
+  var app, deps;
 
-  var app = angular.module(
-    'AbnTest',
-    [
-      'angularBootstrapNavTree',
-      'ngAnimate',
-      'ngMockE2E'
-    ]
-  );
+  deps = ['angularBootstrapNavTree'];
 
-  app.run(function($httpBackend) {
-    $httpBackend.when('GET', '/get-folder-children', {path: '/'}).respond(
-      [
-        {
-          id: 'news',
-          label: 'News',
-          path: '/news',
-          uid: '1234',
-          children: []
-        },
-        {
-          id: 'events',
-          label: 'Events',
-          path: '/events',
-          uid: '4567',
-          children: []
-        }
-      ]
-    );
-    $httpBackend.when('GET', '/get-folder-children', {path: '/news'}).respond(
-      [
-        {
-          id: 'news-1',
-          label: 'News 1',
-          path: '/news/news-1',
-          uid: '1234',
-          children: []
-        },
-        {
-          id: 'news-2',
-          label: 'News 2',
-          path: '/news/news-2',
-          uid: '4567',
-          children: []
-        }
-      ]
-    );
-  });
+  if (angular.version.full.indexOf("1.2") >= 0) {
+    deps.push('ngAnimate');
+  }
 
-  app.factory('backendService',
-    function($http) {
-      var runUserRequest = function(path) {
-        return $http({
-          method: 'GET',
-          url: '/get-folder-children',
-          data: {
-            path: path
-          }
-        });
-      };
-      return {
-        children: function(path) {
-          return runUserRequest(path);
-        }
-      };
-    }
-  );
+  app = angular.module('AbnTest', deps);
 
-  app.controller('AbnTestController', function($scope, $timeout, backendService) {
-
-    $scope.my_data = [
-      {
-        label: 'North America',
-        children: []
-      }, {
-        label: 'South America',
-        children: []
-      }
-    ];
-
-    backendService.children('/').success(function(data, status) {
-      $scope.my_data = data;
-      console.log(data);
-    });
-
-
+  app.controller('AbnTestController', function($scope, $timeout) {
+    var apple_selected, tree, treedata_avm, treedata_geography;
     $scope.my_tree_handler = function(branch) {
       var _ref;
       $scope.output = "You selected: " + branch.label;
-      if (branch.label == 'North America') {
-        branch.children = [
+      if ((_ref = branch.data) != null ? _ref.description : void 0) {
+        return $scope.output += '(' + branch.data.description + ')';
+      }
+    };
+    apple_selected = function(branch) {
+      return $scope.output = "APPLE! : " + branch.label;
+    };
+    treedata_avm = [
+      {
+        label: 'Animal',
+        children: [
+          {
+            label: 'Dog',
+            data: {
+              description: "man's best friend"
+            }
+          }, {
+            label: 'Cat',
+            data: {
+              description: "Felis catus"
+            }
+          }, {
+            label: 'Hippopotamus',
+            data: {
+              description: "hungry, hungry"
+            }
+          }, {
+            label: 'Chicken',
+            children: ['White Leghorn', 'Rhode Island Red', 'Jersey Giant']
+          }
+        ]
+      }, {
+        label: 'Vegetable',
+        data: {
+          definition: "A plant or part of a plant used as food, typically as accompaniment to meat or fish, such as a cabbage, potato, carrot, or bean.",
+          data_can_contain_anything: true
+        },
+        onSelect: function(branch) {
+          return $scope.output = "Vegetable: " + branch.data.definition;
+        },
+        children: [
+          {
+            label: 'Oranges'
+          }, {
+            label: 'Apples',
+            children: [
+              {
+                label: 'Granny Smith',
+                onSelect: apple_selected
+              }, {
+                label: 'Red Delicous',
+                onSelect: apple_selected
+              }, {
+                label: 'Fuji',
+                onSelect: apple_selected
+              }
+            ]
+          }
+        ]
+      }, {
+        label: 'Mineral',
+        children: [
+          {
+            label: 'Rock',
+            children: ['Igneous', 'Sedimentary', 'Metamorphic']
+          }, {
+            label: 'Metal',
+            children: ['Aluminum', 'Steel', 'Copper']
+          }, {
+            label: 'Plastic',
+            children: [
+              {
+                label: 'Thermoplastic',
+                children: ['polyethylene', 'polypropylene', 'polystyrene', ' polyvinyl chloride']
+              }, {
+                label: 'Thermosetting Polymer',
+                children: ['polyester', 'polyurethane', 'vulcanized rubber', 'bakelite', 'urea-formaldehyde']
+              }
+            ]
+          }
+        ]
+      }
+    ];
+    treedata_geography = [
+      {
+        label: 'North America',
+        children: [
           {
             label: 'Canada',
             children: ['Toronto', 'Vancouver']
@@ -101,10 +111,10 @@
             label: 'Mexico',
             children: ['Mexico City', 'Guadalajara']
           }
-        ];
-      }
-      if (branch.label == 'South America') {
-        branch.children = [
+        ]
+      }, {
+        label: 'South America',
+        children: [
           {
             label: 'Venezuela',
             children: ['Caracas', 'Maracaibo']
@@ -115,13 +125,31 @@
             label: 'Argentina',
             children: ['Buenos Aires', 'Cordoba']
           }
-        ];
+        ]
       }
-      if ((_ref = branch.data) !== null ? _ref.description : void 0) {
-        return $scope.output += '(' + branch.data.description + ')';
+    ];
+    $scope.my_data = treedata_avm;
+    $scope.try_changing_the_tree_data = function() {
+      if ($scope.my_data === treedata_avm) {
+        return $scope.my_data = treedata_geography;
+      } else {
+        return $scope.my_data = treedata_avm;
       }
     };
-
+    $scope.my_tree = tree = {};
+    $scope.try_async_load = function() {
+      $scope.my_data = [];
+      $scope.doing_async = true;
+      return $timeout(function() {
+        if (Math.random() < 0.5) {
+          $scope.my_data = treedata_avm;
+        } else {
+          $scope.my_data = treedata_geography;
+        }
+        $scope.doing_async = false;
+        return tree.expand_all();
+      }, 1000);
+    };
     return $scope.try_adding_a_branch = function() {
       var b;
       b = tree.get_selected_branch();
@@ -133,7 +161,6 @@
         }
       });
     };
-
   });
 
 }).call(this);
