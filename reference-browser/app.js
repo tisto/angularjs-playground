968d7006a212a58b0ea24433e6587a4c4dc6588b
Timo Stollenwerk <contact@timostollenwerk.net>
2014-12-15 07:57:20 +0100

    Cleanup.

--- reference-browser/app.js
+++ reference-browser/app.js
@@ -108,59 +108,8 @@
         ]
       }
     ];
-    treedata_geography = [
-      {
-        label: 'North America',
-        children: [
-          {
-            label: 'Canada',
-            children: ['Toronto', 'Vancouver']
-          }, {
-            label: 'USA',
-            children: ['New York', 'Los Angeles']
-          }, {
-            label: 'Mexico',
-            children: ['Mexico City', 'Guadalajara']
-          }
-        ]
-      }, {
-        label: 'South America',
-        children: [
-          {
-            label: 'Venezuela',
-            children: ['Caracas', 'Maracaibo']
-          }, {
-            label: 'Brazil',
-            children: ['Sao Paulo', 'Rio de Janeiro']
-          }, {
-            label: 'Argentina',
-            children: ['Buenos Aires', 'Cordoba']
-          }
-        ]
-      }
-    ];
     $scope.my_data = treedata_avm;
-    $scope.try_changing_the_tree_data = function() {
-      if ($scope.my_data === treedata_avm) {
-        return $scope.my_data = treedata_geography;
-      } else {
-        return $scope.my_data = treedata_avm;
-      }
-    };
     $scope.my_tree = tree = {};
-    $scope.try_async_load = function() {
-      $scope.my_data = [];
-      $scope.doing_async = true;
-      return $timeout(function() {
-        if (Math.random() < 0.5) {
-          $scope.my_data = treedata_avm;
-        } else {
-          $scope.my_data = treedata_geography;
-        }
-        $scope.doing_async = false;
-        return tree.expand_all();
-      }, 1000);
-    };
     return $scope.try_adding_a_branch = function() {
       var b;
       b = tree.get_selected_branch();
