0adc7bead8fd3ca1feb9fe7cddb047bc8b37643d
Timo Stollenwerk <contact@timostollenwerk.net>
2015-01-20 18:31:01 +0100

    Use grid options.

--- ui-grid/app.js
+++ ui-grid/app.js
@@ -4,25 +4,82 @@
 
   angular.module('myApp').controller('TableController',
     function($scope) {
-      $scope.myData = [
+      $scope.gridOptions = {};
+
+      $scope.gridOptions.columnDefs = [
+        {name: 'id'},
+        {name: 'name'},
+        {name: 'age'},
+        {name: 'address.city'}
+      ];
+
+      $scope.gridOptions.data = [
         {
-          "firstName": "Cox",
-          "lastName": "Carney",
-          "company": "Enormo",
-          "employed": true
+          "id": 0,
+          "name": "Ramsey Cummings",
+          "gender": "male",
+          "age": 52,
+          "address":
+          {
+            "state": "South Carolina",
+            "city": "Glendale"
+          }
         },
         {
-          "firstName": "Lorraine",
-          "lastName": "Wise",
-          "company": "Comveyer",
-          "employed": false
+          "id": 1,
+          "name": "Stefanie Huff",
+          "gender": "female",
+          "age": 70,
+          "address":
+          {
+            "state": "Arizona",
+            "city": "Beaverdale"
+          }
         },
         {
-          "firstName": "Nancy",
-          "lastName": "Waters",
-          "company": "Fuelton",
-          "employed": false
-        }
+          "id": 2,
+          "name": "Mabel David",
+          "gender": "female",
+          "age": 52,
+          "address":
+          {
+            "state": "New Mexico",
+            "city": "Grazierville"
+          }
+        },
+        {
+            "id": 3,
+            "name": "Frank Bradford",
+            "gender": "male",
+            "age": 61,
+            "address":
+            {
+              "state": "Wisconsin",
+              "city": "Saranap"
+            }
+        },
+        {
+            "id": 4,
+            "name": "Forbes Levine",
+            "gender": "male",
+            "age": 34,
+            "address":
+            {
+              "state": "Vermont",
+              "city": "Norris"
+            }
+        },
+        {
+          "id": 5,
+          "name": "Santiago Mcclain",
+          "gender": "male",
+          "age": 38,
+          "address":
+          {
+            "state": "Montana",
+            "city": "Bordelonville"
+          }
+        },
       ];
     }
   );
