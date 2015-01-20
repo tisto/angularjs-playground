36f3b4a7137941b29538667a0b10daed22e6f5b5
Timo Stollenwerk <contact@timostollenwerk.net>
2015-01-20 18:19:19 +0100

    Add new ui-grid.

--- ui-grid/app.js
+++ ui-grid/app.js
@@ -1,26 +1,29 @@
+(function() {
+  'use strict';
+  angular.module('myApp', ['ui.grid']);
 
-var app = angular.module('app', ['ui.grid']);
-
-app.controller('MainCtrl', ['$scope', function ($scope) {
-
-  $scope.myData = [
-    {
-    "firstName": "Cox",
-    "lastName": "Carney",
-    "company": "Enormo",
-    "employed": true
-    },
-    {
-    "firstName": "Lorraine",
-    "lastName": "Wise",
-    "company": "Comveyer",
-    "employed": false
-    },
-    {
-    "firstName": "Nancy",
-    "lastName": "Waters",
-    "company": "Fuelton",
-    "employed": false
+  angular.module('myApp').controller('TableController',
+    function($scope) {
+      $scope.myData = [
+        {
+          "firstName": "Cox",
+          "lastName": "Carney",
+          "company": "Enormo",
+          "employed": true
+        },
+        {
+          "firstName": "Lorraine",
+          "lastName": "Wise",
+          "company": "Comveyer",
+          "employed": false
+        },
+        {
+          "firstName": "Nancy",
+          "lastName": "Waters",
+          "company": "Fuelton",
+          "employed": false
+        }
+      ];
     }
-  ];
-}]);
+  );
+})();
