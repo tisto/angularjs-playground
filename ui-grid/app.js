03243ed02ff29ea5ac3cd500b979630e2ecd7686
Balazs Ree <ree@greenfinity.hu>
2015-02-13 15:11:39 +0100

    Minor cleanup

--- ui-grid/app.js
+++ ui-grid/app.js
@@ -46,26 +46,19 @@
         {name: 'age'}
       ];
 
-      var batch_start = 0;
       var batch_size = 50;
 
-      var timeout;
-      if (timeout) $timeout.cancel(timeout);
-      timeout = $timeout(function() {
-        usersService.users(batch_start, batch_size)
-        .success(function(data, status) {
-          $scope.gridOptions.data = data;
-          batch_start = batch_start + batch_size;
-        });
-      }, 350);
+      usersService.users(0, batch_size)
+      .success(function(data, status) {
+        $scope.gridOptions.data = data;
+      });
 
       $scope.gridOptions.onRegisterApi = function(gridApi){
         gridApi.infiniteScroll.on.needLoadMoreData($scope,function(){
-          usersService.users(batch_start, batch_size)
+          usersService.users($scope.gridOptions.data.length, batch_size)
           .success(function(data, status) {
             $scope.gridOptions.data = $scope.gridOptions.data.concat(data);
             gridApi.infiniteScroll.dataLoaded();
-            batch_start = batch_start + batch_size;
           }).error(function() {
             gridApi.infiniteScroll.dataLoaded();
           });
