e36812ae9a042f75db5d02372bfe976742e33302
Timo Stollenwerk <contact@timostollenwerk.net>
2015-02-23 15:05:18 +0100

    Add less; refactor.

--- _start/gulpfile.js
+++ _start/gulpfile.js
@@ -1,16 +1,44 @@
 (function() {
   'use strict';
-  var gulp        = require('gulp');
-  var browserSync = require('browser-sync');
+
+  // --- DEPENDENCIES --------------------------------------------------------
+  var gulp = require('gulp'),
+      sourcemaps = require('gulp-sourcemaps'),
+      header = require('gulp-header'),
+      browserSync = require('browser-sync'),
+      less = require('gulp-less'),
+      uglify = require('gulp-uglify'),
+      watch = require('gulp-watch');
 
 
-  // --- WATCH -----------------------------------------------------------------
-  gulp.task('watch', function() {
-    gulp.watch('**/*.*', ['browser-sync']);
+  // --- HTML ----------------------------------------------------------------
+  gulp.task('html', function() {
+    gulp.src('index.html')
+        .pipe(gulp.dest('./build/'))
+        .pipe(browserSync.reload({stream:true, once: true}));
   });
 
 
-  // --- BROWSER SYNC ----------------------------------------------------------
+  // --- JAVASCRIPT ----------------------------------------------------------
+  gulp.task('js', function() {
+    gulp.src('./src/script.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./build/'))
+        .pipe(browserSync.reload({stream:true, once: true}));
+  });
+
+
+  // --- LESS ----------------------------------------------------------------
+  gulp.task('less', function() {
+    gulp.src('src/less/*.less')
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .pipe(gulp.dest('build/css'))
+        .pipe(browserSync.reload({stream:true, once: true}));
+  });
+
+
+  // --- BROWSER SYNC --------------------------------------------------------
   gulp.task('browser-sync', function() {
     browserSync({
       server: {
@@ -23,9 +51,25 @@
   });
 
 
-  // --- DEFAULT ---------------------------------------------------------------
-  gulp.task('default', ['browser-sync'], function () {
+  // --- WATCH ---------------------------------------------------------------
+  gulp.task('watch', function() {
+    gulp.watch('./src/less/*.less', function(){
+      gulp.run('less');
+    });
+    gulp.watch('./src/*.html', function(){
+      gulp.run('html');
+    });
+    gulp.watch('./src/*.js', function(){
+      gulp.run('js');
+    });
+
+  });
+
+
+  // --- DEFAULT -------------------------------------------------------------
+  gulp.task('default', ['less', 'html', 'js', 'watch', 'browser-sync'], function() {
     gulp.watch("*.*", ['browser-sync-reload']);
+    gulp.watch('src/less/*.less', ['less']);
   });
 
 })();
