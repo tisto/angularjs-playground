97d853fd1f43173ac11c8ae7a51d629c2d877202
Timo Stollenwerk <tisto@plone.org>
2015-03-05 08:00:38 +0100

    Add angular-strap-typeahead.

---
+++ angular-strap-typeahead/app.js
@@ -0,0 +1,15 @@
+angular.module(
+    'myApp',
+    [
+        'ngAnimate',
+        'mgcrea.ngStrap',
+        'mgcrea.ngStrap.typeahead',
+        'mgcrea.ngStrap.tooltip'
+    ]
+);
+
+angular.module('myApp').controller('TypeaheadController', function($scope) {
+  'use strict';
+  $scope.selectedState = "";
+  $scope.states = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Dakota","North Carolina","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"];
+});
