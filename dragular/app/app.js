cc5fc27f50fd23343049f70a611967ab1ce00c96
Timo Stollenwerk <tisto@plone.org>
2015-08-13 08:24:49 +0200

    Nested with angular model.

--- dragular/app/app.js
+++ dragular/app/app.js
@@ -3,29 +3,69 @@
 
   angular.module('myApp', ['angular-loading-bar', 'dragularModule']);
 
-  angular.module('myApp').controller('Basic', ['$scope', '$element', 'dragularService', function TodoCtrl($scope, $element, dragularService) {
-    $scope.items1 = [{
-      content: 'Move me, but you can only drop me in one of these containers.'
+  angular.module('myApp').controller('Basic', ['$timeout', '$scope', '$element', 'dragularService', function TodoCtrl($timeout, $scope, $element, dragularService) {
+    $timeout(function() { // timeount due to nested ngRepeat to be ready
+      var container = $element.children().eq(0).children(),
+        parentContainers = container.children(),
+        nestedContainers = [];
+
+      dragularService(container, {
+        moves: function(el, container, handle) {
+          return handle.classList.contains('row-handle');
+        },
+        containersModel: $scope.items
+      });
+
+      // collect nested contianers
+      for (var i = 0; i < parentContainers.length; i++) {
+        nestedContainers.push(parentContainers.eq(i).children()[1]);
+      }
+
+      dragularService(nestedContainers, {
+        moves: function(el, container, handle) {
+          return !handle.classList.contains('row-handle');
+        },
+        containersModel: (function getNestedContainersModel(){
+          var parent = $scope.items,
+            containersModel = [];
+          for (var i = 0; i < parent.length; i++) {
+            containersModel.push(parent[i].items);
+          }
+          return containersModel;
+        })()
+      });
+    }, 0);
+    $scope.items = [{
+      items: [{
+        content: 'Item a1'
+      }, {
+        content: 'Item a2'
+      }, {
+        content: 'Item a3'
+      }, {
+        content: 'Item a4'
+      }]
     }, {
-      content: 'If you try to drop me somewhere other than these containers, I\'ll just come back.'
+      items: [{
+        content: 'Item b1'
+      }, {
+        content: 'Item b2'
+      }, {
+        content: 'Item b3'
+      }, {
+        content: 'Item b4'
+      }]
     }, {
-      content: 'Item 3'
-    }, {
-      content: 'Item 4'
+      items: [{
+        content: 'Item c1'
+      }, {
+        content: 'Item c2'
+      }, {
+        content: 'Item c3'
+      }, {
+        content: 'Item c4'
+      }]
     }];
-    $scope.items2 = [{
-      content: 'Item 5'
-    }, {
-      content: 'Item 6'
-    }, {
-      content: 'Item 7'
-    }, {
-      content: 'Item 8'
-    }];
-    var containers = $element.children().children();
-    dragularService([containers[0],containers[1]],{
-      containersModel: [$scope.items1, $scope.items2]
-    });
   }]);
 
 })();
