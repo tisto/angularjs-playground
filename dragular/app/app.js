3a4b907860cb904c89260d8324ca622fcb6d9e4f
Timo Stollenwerk <tisto@plone.org>
2015-08-13 08:19:11 +0200

    Use angular model.

--- dragular/app/app.js
+++ dragular/app/app.js
@@ -3,11 +3,29 @@
 
   angular.module('myApp', ['angular-loading-bar', 'dragularModule']);
 
-  angular.module('myApp').controller('Basic',
-    ['$element', 'dragularService',
-    function TodoCtrl($element, dragularService) {
-      dragularService($element.children());
-    }]
-  )
+  angular.module('myApp').controller('Basic', ['$scope', '$element', 'dragularService', function TodoCtrl($scope, $element, dragularService) {
+    $scope.items1 = [{
+      content: 'Move me, but you can only drop me in one of these containers.'
+    }, {
+      content: 'If you try to drop me somewhere other than these containers, I\'ll just come back.'
+    }, {
+      content: 'Item 3'
+    }, {
+      content: 'Item 4'
+    }];
+    $scope.items2 = [{
+      content: 'Item 5'
+    }, {
+      content: 'Item 6'
+    }, {
+      content: 'Item 7'
+    }, {
+      content: 'Item 8'
+    }];
+    var containers = $element.children().children();
+    dragularService([containers[0],containers[1]],{
+      containersModel: [$scope.items1, $scope.items2]
+    });
+  }]);
 
 })();
