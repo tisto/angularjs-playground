46537a5f2a97f003f4e28419eab351018b47a852
Timo Stollenwerk <tisto@plone.org>
2015-08-28 13:32:55 +0200

    Add more fields; add submit method. Make field required.

--- angular-schema-form/app.js
+++ angular-schema-form/app.js
@@ -7,12 +7,30 @@
     $scope.schema = {
       type: "object",
       properties: {
-        name: { type: "string", minLength: 2, title: "Name", description: "Name or alias" },
+        name: {
+          type: "string",
+          minLength: 2,
+          title: "Fullname",
+          description: "Fullname"
+        },
+        email: {
+          title: "Email",
+          type: "string",
+          pattern: "^\\S+@\\S+$",
+          description: "Email will be used for evil."
+        },
+        spam: {
+          title: "Spam me, please",
+          type: "boolean"
+        },
         title: {
           type: "string",
           enum: ['dr', 'jr', 'sir', 'mrs', 'mr', 'NaN', 'dj']
         }
-      }
+      },
+      required: [
+        'name'
+      ],
     };
 
     $scope.form = [
@@ -25,6 +43,15 @@
 
     $scope.model = {};
 
+    $scope.onSubmit = function(form) {
+      // First we broadcast an event so all fields validate themselves
+      $scope.$broadcast('schemaFormValidate');
+
+      // Then we check if the form is valid
+      if (form.$valid) {
+        console.log('check if form is valid')
+      }
+    }
   });
 
 })();
