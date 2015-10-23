792a3398349dff291a91bc0c0403def2dc751d60
Timo Stollenwerk <tisto@plone.org>
2015-10-23 16:38:11 +0200

    Remove unnecessary code.

--- webpack-starter-angular/src/a.js
+++
@@ -1,7 +0,0 @@
-import $ from 'jquery';
-
-export function addButton(el) {
-  el.append($(`<button type="button" class="btn btn-default">
-                <span class="glyphicon glyphicon-ok-sign"></span>
-              </button>`));
-}
--- webpack-starter-angular/src/index.js
+++ webpack-starter-angular/src/index.js
@@ -1,24 +1,11 @@
 import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {addButton} from './a.js';
-import {Person} from './Person.js';
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import AppComponent from './app.component.js';
 import Common from './common/common';
 import Components from './components/components';
 import Styles from './styles.scss';
-
-$(document).ready(() => {
-  addButton($('#root'));
-});
-
-
-global.app = function () {
-  var timo = new Person('Timo', 'Stollenwerk');
-  console.log(timo.fullName);
-};
-
 
 angular.module('myApp', [
   uiRouter,
