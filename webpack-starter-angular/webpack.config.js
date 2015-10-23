6146273f83a66c515d2fc065c37485b22de699c6
Timo Stollenwerk <tisto@plone.org>
2015-10-23 16:45:27 +0200

    index.js -> app.js.

--- webpack-starter-angular/webpack.config.js
+++ webpack-starter-angular/webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/app.js',
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
