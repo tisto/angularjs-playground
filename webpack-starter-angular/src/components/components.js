2f6eff1491ed5a0f7c78c8e323b266a6c4294341
Timo Stollenwerk <tisto@plone.org>
2015-11-04 12:11:49 +0100

    Add two nested ui-router states in contact.

--- webpack-starter-angular/src/components/components.js
+++ webpack-starter-angular/src/components/components.js
@@ -2,10 +2,14 @@ import angular from 'angular';
 import Home from './home/home';
 import About from './about/about';
 import Contact from './contact/contact';
+import ContactAddress from './contact/contact.address';
+import ContactForm from './contact/contact.form';
 
 let componentModule = angular.module('app.components', [
   About.name,
   Contact.name,
+  ContactAddress.name,
+  ContactForm.name,
   Home.name,
 ]);
 
