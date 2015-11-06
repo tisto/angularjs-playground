fcc885d843371677719c81a75a1a7b83ae762972
Timo Stollenwerk <tisto@plone.org>
2015-11-06 15:27:59 +0100

    Make modal example work.

--- webpack-angular-strap/src/app.js
+++ webpack-angular-strap/src/app.js
@@ -1,8 +1,13 @@
 import angular from 'angular';
 import AngularStrap from 'angular-strap';
+import AngularStrapTpl from 'angular-strap/dist/angular-strap.tpl';
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
+
 import MyButton from './mybutton';
+import MyModal from './mymodal';
 
 angular.module('myApp', [
   'mgcrea.ngStrap',
-  MyButton.name
+  MyButton.name,
+  MyModal.name
 ]);
